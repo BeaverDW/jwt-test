59d6a90af9fc7af03f7b338bf222280decedfd4f
Beaver <BeaverDW.92@gmail.com>
2024-05-09 10:18:56 +0900

    init project

---
+++ src/test/java/com/jwt/testjwt/TestjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.testjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
